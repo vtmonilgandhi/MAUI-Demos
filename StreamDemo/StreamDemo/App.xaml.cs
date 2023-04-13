ba54196c9ff70ecc3b574e169f76addd9ebd735a
Monil Gandhi <monil.gandhi@volansys.com>
2023-04-13 11:50:09 +0530

    Added demos

---
+++ StreamDemo/StreamDemo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StreamDemo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ StreamDemo/StreamDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StreamDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
